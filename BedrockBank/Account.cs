060b8cff86f3644dfd303ef47aca29d6dc1d69d4
NLSAKARANTH <NLSAKARANTH@yahoo.com>
2017-03-19 10:14:08 -0700

    Static Variable

--- BedrockBank/Account.cs
+++ BedrockBank/Account.cs
@@ -8,11 +8,15 @@ namespace BedrockBank
 {
     class Account
     {
+        #region Static Varialbes
+        private static int lastAccoubtNumber = 0;
+        #endregion
+
         #region Properties
         /// <summary>
         /// Account number of the account
         /// </summary>
-        public int AccountNumber { get;}
+        public int AccountNumber { get; private set; }
         /// <summary>
         /// Email address of the account holder
         /// </summary>
@@ -20,6 +24,14 @@ namespace BedrockBank
 
         public decimal Balance { get;  private set; }
         public string TypeOfAccount { get; set; }
+        #endregion
+
+        #region Constructor
+        public Account()
+        {
+            //lastAccountNumber ++1
+            AccountNumber = ++lastAccoubtNumber;
+        }
         #endregion
 
         #region Methods
--- BedrockBank/Program.cs
+++ BedrockBank/Program.cs
@@ -10,11 +10,41 @@ namespace BedrockBank
     {
         static void Main(string[] args)
         {
-           var account1 =  new Account();
-            account1.EmailAddress = "test@test.com";
-            account1.TypeOfAccount = "Checking";
-            account1.Deopsit(105.40M);
-            Console.WriteLine($"Accountnumber: {account1.AccountNumber}, EmailAddress: {account1.EmailAddress}, TypeOfAccount: {account1.TypeOfAccount}, Balance: {account1.Balance:C}");
+
+            Console.WriteLine("************Welcome to Bedrock Bank!***********");
+            Console.WriteLine("Please select from the following option");
+            Console.WriteLine("0.Exit");
+            Console.WriteLine("1. Create an account");
+            Console.WriteLine("2.Deposit");
+            Console.WriteLine("3.Withdraw");
+            var option = Console.ReadLine();
+            switch (option)
+            {
+                case "0":
+                    Console.WriteLine("Thank you for visiting! Good bye!");
+                    break;
+                case "1":
+                    var account1 = new Account();
+                    Console.Write("Please provide your email address: ");
+                    var emailAddress = Console.ReadLine();
+                    Console.Write("What type of account you need?");
+                    var typeOfAccount = Console.ReadLine();
+
+
+                    account1.EmailAddress = emailAddress;
+                    account1.TypeOfAccount = "Checking";
+                    
+                   Console.WriteLine($"Accountnumber: {account1.AccountNumber}, EmailAddress: {account1.EmailAddress}, TypeOfAccount: {account1.TypeOfAccount}, Balance: {account1.Balance:C}");
+                    break;
+                case "2":
+                case "3":
+               
+                default:
+                    break;
+            }
+
+
+            
             
           
         
