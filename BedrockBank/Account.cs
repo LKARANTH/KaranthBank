db7506fe268992481f653ec666128eac7fdd333e
NLSAKARANTH <NLSAKARANTH@yahoo.com>
2017-03-19 09:31:08 -0700

    Check in

---
+++ BedrockBank/Account.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BedrockBank
+{
+    class Account
+    {
+        #region Properties
+        /// <summary>
+        /// Account number of the account
+        /// </summary>
+        public int AccountNumber { get;}
+        /// <summary>
+        /// Email address of the account holder
+        /// </summary>
+        public string EmailAddress { get; set; }
+
+        public decimal Balance { get;  private set; }
+        public string TypeOfAccount { get; set; }
+        #endregion
+
+        #region Methods
+        public void Deopsit(decimal amount)
+        {
+            //Balance = Balance + amount;
+            Balance += amount;
+        }
+        public void Withdraw(decimal amount)
+        {
+            Balance -= amount;
+        }
+        #endregion
+    }
+
+}
---
+++ BedrockBank/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BedrockBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var account1 =  new Account();
+            account1.EmailAddress = "test@test.com";
+            account1.TypeOfAccount = "Checking";
+            account1.Deopsit(105.40M);
+            Console.WriteLine($"Accountnumber: {account1.AccountNumber}, EmailAddress: {account1.EmailAddress}, TypeOfAccount: {account1.TypeOfAccount}, Balance: {account1.Balance:C}");
+            
+          
+        
+        }
+    }
+}
